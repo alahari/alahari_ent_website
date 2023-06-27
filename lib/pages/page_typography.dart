71728c35fde630b09b9a13ecdb4f4819aed5e9eb
Venki Alahari <venkat.alahari@gmail.com>
2023-06-27 20:39:25 +0530

    careers page

--- lib/pages/page_typography.dart
+++ lib/pages/page_typography.dart
@@ -15,78 +15,20 @@ class TypographyPage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   const MenuBar(),
-            /*      Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      margin: marginBottom12,
-                      child: Text("Typography", style: headlineTextStyle),
-                    ),
+                  ListTile(
+                    title: Text('Job Position 1'),
+                    subtitle: Text('Sales Representative'),
+                    onTap: () {
+                      // Handle job position 2 tap
+                    },
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      margin: marginBottom24,
-                      child: Text("Text styles for pages and posts.",
-                          style: subtitleTextStyle),
-                    ),
+                  ListTile(
+                    title: Text('Job Position 2'),
+                    subtitle: Text('Electrical Technician'),
+                    onTap: () {
+                      // Handle job position 3 tap
+                    },
                   ),
-                  divider,
-                  Container(
-                    margin: marginBottom40,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      margin: marginBottom12,
-                      child: Text("Basic Styles",
-                          style: headlineSecondaryTextStyle),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      margin: marginBottom24,
-                      child: Text("Simple to remember and use",
-                          style: subtitleTextStyle),
-                    ),
-                  ),
-                  dividerSmall,
-                  Container(
-                    margin: marginBottom24,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      margin: marginBottom24,
-                      child: Text("Headline", style: headlineTextStyle),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      margin: marginBottom24,
-                      child: Text("Headline Secondary",
-                          style: headlineSecondaryTextStyle),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      margin: marginBottom24,
-                      child: Text("Subtitle", style: subtitleTextStyle),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      margin: marginBottom40,
-                      child: Text(
-                          "Body text is the default text style. Use this text style for website content and paragraphs. This text is chosen to be easy and comfortable to read. As the default text style for large blocks of text, particular attention is placed on the choice of font. Some fonts are more comfortable to read than others.",
-                          style: bodyTextStyle),
-                    ),
-                  ),
-                  divider,
-                  const Footer(), */
                 ],
               ),
             ),
