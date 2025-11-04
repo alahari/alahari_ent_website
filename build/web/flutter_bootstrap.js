418d844801ec0ccf7bbbd446afd9cd336b648f12
sasank2329 <sasank232704@gmail,com>
2025-11-04 18:59:51 +0530

    Add updated web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1408698863"
+    serviceWorkerVersion: "3212287051"
   }
 });
